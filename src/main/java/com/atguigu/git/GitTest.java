a5db422619434654ae4ef8aa587674634c8148d1
DBAALI <129338974+DBAALI@users.noreply.github.com>
2023-03-30 06:56:23 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello,Git1");
         System.out.println("Hello,Git2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
